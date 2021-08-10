b653b310e285e908827440cb1525c7d852f80a9d
malinjr07 <malinjr07@gmail.com>
2021-08-10 15:41:07 +0600

    services mobile done

--- src/pages/homePage/services/mobile.jsx
+++ src/pages/homePage/services/mobile.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 // dummy data
 import { services } from '../../../components/data/data';
-import CommonHead from "../../../components/headings/index";
+import CommonHead, {MiniHead} from "../../../components/headings/index";
 
 function Mobile() {
     const [data, setData] = useState([]);
@@ -16,28 +16,25 @@ function Mobile() {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="service_sliders">
-                            <div className="serv_slide">
+                        {data.map((nusrat,rizu)=>
+                            <div key={rizu} className="serv_slide">
                                 <div className="content">
                                     <div className="image">
-                                        <img src="images/service.png" alt="images/service.png" className="img-fluid"/>
+                                        <img src={nusrat.image} alt="services" className="img-fluid"/>
                                     </div>
                                     <div className="texts">
-                                        {/* this is mini heading */}
-                                        <div className="title text-center text-capitalize">
-                                            <h4>Virtual Home Staging</h4>
-                                        </div>
-                                        {/* mini heading ends here */}
+                                        <MiniHead title={nusrat.title}/>
                                         <div className="text_part">
-                                            <h6>PadStyler is the only Photorealistic 3D Visual Marketing Platform backed by
-                                                research to help you sell or rent properties faster</h6>
+                                            <h6>{nusrat.text}</h6>
                                         </div>
-                                        <a href="link" className="mini_btn d-inline-block text-capitalize text-center">
+                                        <a href={nusrat.link} className="mini_btn d-inline-block text-capitalize text-center">
                                             <h5>Learn More</h5>
                                             <i className="fas fa-long-arrow-alt-right"/>
                                         </a>
                                     </div>
                                 </div>
                             </div>
+                        )}
                         </div>
                     </div>
                 </div>
