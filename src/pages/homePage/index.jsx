1f532f2daead31f3409cfeb0aa3e9a1ae6128bd8
malinjr07 <malinjr07@gmail.com>
2021-08-14 23:11:15 +0600

    service page data and loader Done

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect, Suspense, lazy, useContext} from 'react';
+import React, {useState, useEffect, Suspense, lazy, useContext} from 'react';
 
 import Difference from "../../components/difference/difference";
 import PreviousWork from "../../components/previousWork/index";
@@ -10,61 +10,71 @@ import Pertnership from "./Pertnership";
 
 // Dummy Data
 
-import { different, howItWork, services, previousWork, agency, partner } from "../../components/data/data";
+import {
+    different,
+    howItWork,
+    services,
+    previousWork,
+    agency,
+    partner
+} from "../../components/data/data";
 import Loaders from '../../components/contexts';
 
-const Banner=lazy(()=>import('./banner/main'))
-
+const Banner = lazy(() => import ('./banner/main'))
 
 function Home() {
 
-const [dif, setDif] = useState([]);
-const [hiw, setHiw] = useState([]);
-const [serv, setServ] = useState([]);
-const [prevWork, setprevWork] = useState([]);
-const [agent, setAgent] = useState([]);
-const [partnership, setPartnership] = useState([]);
+    const [dif,
+        setDif] = useState([]);
+    const [hiw,
+        setHiw] = useState([]);
+    const [serv,
+        setServ] = useState([]);
+    const [prevWork,
+        setprevWork] = useState([]);
+    const [agent,
+        setAgent] = useState([]);
+    const [partnership,
+        setPartnership] = useState([]);
 
-// Global Loader
+    // Global Loader
 
-    const { loader, updateLoader } = useContext(Loaders);
+    const {loader, updateLoader} = useContext(Loaders);
 
-useEffect(() => {
-    setDif(different);
-    setHiw(howItWork);
-    setServ(services);
-    setprevWork(previousWork);
-    setAgent(agency);
-    setPartnership(partner);
-    setTimeout(() => {
-        updateLoader(false);
-    }, 2000);
-    return () => {
-        updateLoader(true);
-    };
-    // eslint-disable-next-line
-}, []);
+    useEffect(() => {
+        setDif(different);
+        setHiw(howItWork);
+        setServ(services);
+        setprevWork(previousWork);
+        setAgent(agency);
+        setPartnership(partner);
+        setTimeout(() => {
+            updateLoader(false);
+        }, 2000);
+        return () => {
+            updateLoader(true);
+        };
+        // eslint-disable-next-line
+    }, []);
 
-    return (
-        <>
-{loader===true ? 'Loading...':
-        <section className="home_page">
-        <Suspense fallback={<p>Please Wait...</p>}>
-            <Banner/>
-        </Suspense>
-        <Difference
-            title='What set us apart from other virtual staging companies'
-                rare={dif}/>
-            <PreviousWork data={prevWork}/>
-            <Services data={serv} />
-        {/* Incomplete Mobile Part */}
-            <HowItWork title="How it works ?" work={hiw}/>
-        <Pricing/>
-            <Agencies data={agent} />
-            <Pertnership data={partnership} />
-        </section>
-}
-</>
+    return ( <> {
+        loader === true
+            ? 'Loading...'
+            : <section className="home_page">
+                    <Suspense fallback={< p > Please Wait ...</p>}>
+                        <Banner/>
+                    </Suspense>
+                    <Difference
+                        title='What set us apart from other virtual staging companies'
+                        rare={dif}/>
+                    <PreviousWork data={prevWork}/>
+                    <Services data={serv}/> {/* Incomplete Mobile Part */}
+                    <HowItWork title="How it works ?" work={hiw}/>
+                    <Pricing/>
+                    <Agencies data={agent}/>
+                    <Pertnership data={partnership}/>
+                </section>
+    } </>
     )
 }
 
--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Banner from "../../components/banner/index";
 import Difference from "../../components/difference/difference";
 import PreviousWork from "../../components/previousWork/index";
 import HowItWork from "../../components/HowItWork/index";
 // Dummy Data
-import imgUrl, { url,give,take,faq } from "../../components/data/data";
+import imgUrl, { url, give, take, faq, previousWork} from "../../components/data/data";
+import Loaders from '../../components/contexts';
 
 function Services() {
-    return (
-        <section className="service_page">
-            <Banner title='Virtual Home Staging' text='We specialize in transforming photos of vacant properties into beautiful, virtually staged homes that sells faster and for top dollar.' price='500' img={`${imgUrl}/banner_banner_bg.jpg`} url={url} buttonText='Place Order' />
-            <Difference title="What we need" need={take} />
-            <Difference title="What will you receive" receive={give} />
-            <PreviousWork/>
-            <HowItWork title="FAQ's" frequents={faq} />
-        </section>
+
+    const [accept,
+        setAccept] = useState([]);
+    const [provide,
+        setProvides] = useState([]);
+    const [faqs,
+        setFaqs] = useState([]);
+    const [pervWork, setPervWork] = useState([]);
+
+    const {loader, updateLoader} = useContext(Loaders);
+
+    useEffect(() => {
+        setAccept(give);
+        setProvides(take);
+        setFaqs(faq);
+        setPervWork(previousWork)
+        setTimeout(() => {
+            updateLoader(false)
+        }, 2000);
+        return () => {
+            updateLoader(true)
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    return ( <> {loader===true ? '' : <section className="service_page">
+        <Banner
+            title='Virtual Home Staging'
+            text='We specialize in transforming photos of vacant properties into beautiful, virtually staged homes that sells faster and for top dollar.'
+            price='500'
+            img={`${imgUrl}/banner_banner_bg.jpg`}
+            url={url}
+            buttonText='Place Order'/>
+        <Difference title="What we need" need={accept}/>
+        <Difference title="What will you receive" receive={provide}/>
+        <PreviousWork data={pervWork}/>
+        <HowItWork title="FAQ's" frequents={faqs}/>
+    </section>}  </>
     )
 }
 
